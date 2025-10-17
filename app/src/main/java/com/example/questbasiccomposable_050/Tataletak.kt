634b8097e727addd4f19a87228403394ee8ac3f1
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-17 15:46:41 +0700

    Tata Letak Column dan Row Baris ke 2

--- app/src/main/java/com/example/questbasiccomposable_050/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_050/Tataletak.kt
@@ -51,7 +51,17 @@ fun TataletakColumnRow(modifier: Modifier) {
         //Baris1
         Row(modifier = modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+        }
 
+        //Baris2
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
         }
     }
 }
