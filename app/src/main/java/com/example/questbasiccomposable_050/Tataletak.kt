e0016250d23d93e78dd75dd7c5ad0530fa774a4e
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-17 16:01:43 +0700

    Tata Letak Box, Column, Row: Modifikasi Box

--- app/src/main/java/com/example/questbasiccomposable_050/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_050/Tataletak.kt
@@ -1,13 +1,18 @@
 package com.example.questbasiccomposable_050
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 
@@ -81,6 +86,23 @@ fun tataletakRowColumn(modifier: Modifier) {
             Text(text = "Komponen2kolom2")
             Text(text = "Komponen3kolom3")
         }
+    }
+}
+
+@Composable
+fun TataletakBoxColumnRow(modifier: Modifier) {
+    val gambar = painterResource(id = R.drawable.notasibalok)
+    Column {
+        Box(
+            modifier = modifier.fillMaxWidth().height(height = 110.dp).background(color = Color.Yellow),
+            contentAligment = Aligment.Center
+        ) {
+            Column() {
+                modifier = modifier.fil
+            }
+        }
+
+
     }
 }
 
