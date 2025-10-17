9e2c10d0d67e91b93972a20409d585af0c6808e2
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-17 16:20:22 +0700

    Add styled box with vertical spacer

--- app/src/main/java/com/example/questbasiccomposable_050/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_050/Tataletak.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -118,9 +119,15 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                     Text(text = "Coll1_Row2_Komponen3")
                 }
         }
-
-
     }
+        Spacer(modifier = Modifier.height(height = 10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        )
 }
 
 
